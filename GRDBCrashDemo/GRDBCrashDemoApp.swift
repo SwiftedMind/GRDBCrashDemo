aa68e30e2031d8b78a2d892da1470b0647a931a9
Dennis Müller <d3mueller@me.com>
2024-08-22 15:48:32 +0200

    Initial Commit

---
+++ GRDBCrashDemo/GRDBCrashDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRDBCrashDemoApp.swift
+//  GRDBCrashDemo
+//
+//  Created by Dennis Müller on 22.08.24.
+//
+
+import SwiftUI
+
+@main
+struct GRDBCrashDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
